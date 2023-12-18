4ed8043b3af30e3fe635a2639b4703b10c7eb6d3
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2023-12-18 20:08:29 +0330

    Update _worker.js

--- serverless Cl workers/_worker.js
+++ serverless Cl workers/_worker.js
@@ -1,4 +1,4 @@
-// <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END--> Edit by NiREvil ---- Thanks to 3Kmfi6HP Dear emotional damage :) .
+// time is 2023-12-18 16:32:42 UTC , Edit by NiREvil --> Thanks to 3Kmfi6HP, Dear emotional damage :) --> https://github.com/3Kmfi6HP
 // @ts-ignore
 import { connect } from 'cloudflare:sockets';
 
