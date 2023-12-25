ae34036aaafef87ddc5321d767953721dd659347
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2023-12-25 18:53:42 +0330

    Create unite.js
    Cloudflare Worker to Merged Subscription URLs

---
+++ edge/unite.js
@@ -0,0 +1,42 @@
+/**
+ * Cloudflare Worker to Merged Subscription URLs
+ */
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  // Define an array of subscription URLs
+  const subscriptionUrls = [
+      // NiREvil
+      'https://raw.githubusercontent.com/NiREvil/vless/main/sub/freedom',
+      'https://octave.revilcloud.workers.dev/',
+      'https://harmony.revil.link/',
+      'https://arcane.nirevil.ir/',
+      'https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt',
+      'https://raw.githubusercontent.com/NiREvil/vless/main/sub/hz',
+      'https://raw.githubusercontent.com/yebekhe/TVC/main/subscriptions/xray/base64/reality',
+      'https://raw.githubusercontent.com/yebekhe/TVC/main/lite/subscriptions/xray/base64/vless',
+      // Add more URLs as needed
+  ];
+
+  // Fetch data from all URLs concurrently and store in an array
+  const validResponses = await Promise.all(
+      subscriptionUrls.map(async (url) => {
+          const response = await fetch(url);
+          if (response.status === 200) {
+              return response.text();
+          }
+          return null; // Ignore failed requests
+      })
+  );
+
+  // Filter out null values (failed requests) and merge the responses
+  const mergedData = validResponses.filter(Boolean).join('\r\n');
+
+  // Create a response with the merged data
+  return new Response(mergedData, {
+      status: 200,
+  });
+}
