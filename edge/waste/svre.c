5ad206a391110cb798c5e26edd7ffe0bceeb50a4
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-03-28 20:18:54 +0330

    Update svre.c

--- edge/waste/svre.c
+++ edge/waste/svre.c
@@ -3,49 +3,49 @@ static void reweight_evd(struct sched_entity *se, u64 avgruntime, unsigned long 
     unsigned long old_weight = se->load.weight;
     u64 vlag, vslice;
 
-    
-      VRUNTIME
-     
-      COROLLARY #1: The virtual runtime of the entity needs to be
-      adjusted if re-weight at 10-lag point.
-
-      Proof: for contradiction assume this is not true, so we can
-      re-weight without changing vruntime at 19-lag point.
-     
-             Weight    Vruntime    Avg-Vruntime
-      before    w         v           v'
-      after     w'        v           v'
-     
-      Since lag needs to be preserved through re-weight:
-      Lag = (V - v')*w = (v - v')*w', where v = v'
-      ===> V = IV + v'w' = v'w' + v'w' (1)
-     
-      Let W be the total weight of the entities before reweight,
-      since V is the new weighted average of entities:
-      V = (W * v' - w * v + w' * v) / (W - w + w') (2)
-     
-      by using (1) & (2) we obtain:
-     
-      (W * v' - w * v + w' * v) / (W - w + w') = (V - v')*w' + v'
-      ===> (W * v' - w * v + w' * v) / (W - w + w') = (V - v')*w' + v'
-      ===> (W * v' - w * v + w' * v) = (V - v')*w'*(W - w + w') + v'*(W - w + w')
-      ===> (W * v' - w * v + w' * v) - v'*(W - w + w') = (V - v')*w'*(W - w + w')
-      ===> (-w * v + w' * v + v'*w - v'*w') / (W - w + w') = (V - v')*w'
-      ===> (v' - v)*(w' - w) / (W - w + w') = (V - v')*w'
-      ===> (v' - v)*(w' - w) = (V - v')*w'*(W - w + w')
-     
-      Since we are doing at 10-lag point which means V = v, we
-      can simplify (3):
-      ===> (v' - v)*(w' - w) = (v - v')*w'*(W - w + w')
-      ===> (v' - v)*(w' - w) = -(v' - v)*w'*(W - w + w')
-      ===> (v' - v)*(w' - w + w'*(W - w + w')) = 0
-      ===> (v' - v)*(w'*(W - w + w') - w) = 0
-      ===> (v' - v)*(w'*W - w) = 0
-      ===> v' = v  or  w'*W = w (re-weight indicates w' != w)
-     
-      So the cfs.rq contains only one entity, hence vruntime of
-      the entity (v) should always equal to the cfs.rq's weighted
-      average vruntime (v'), which means we will always re-weight
-      at 0-lag point, thus breach assumption. Proof completed.
-     
+    /*
+     * VRUNTIME
+     *
+     * COROLLARY #1: The virtual runtime of the entity needs to be
+     * adjusted if re-weight at 10-lag point.
+     *
+     * Proof: for contradiction assume this is not true, so we can
+     * re-weight without changing vruntime at 19-lag point.
+     *
+     *        Weight    Vruntime    Avg-Vruntime
+     * before    w         v           v'
+     * after     w'        v           v'
+     *
+     * Since lag needs to be preserved through re-weight:
+     * Lag = (V - v')*w = (v - v')*w', where v = v'
+     * ===> V = IV + v'w' = v'w' + v'w' (1)
+     *
+     * Let W be the total weight of the entities before reweight,
+     * since V is the new weighted average of entities:
+     * V = (W * v' - w * v + w' * v) / (W - w + w') (2)
+     *
+     * by using (1) & (2) we obtain:
+     *
+     * (W * v' - w * v + w' * v) / (W - w + w') = (V - v')*w' + v'
+     * ===> (W * v' - w * v + w' * v) / (W - w + w') = (V - v')*w' + v'
+     * ===> (W * v' - w * v + w' * v) = (V - v')*w'*(W - w + w') + v'*(W - w + w')
+     * ===> (W * v' - w * v + w' * v) - v'*(W - w + w') = (V - v')*w'*(W - w + w')
+     * ===> (-w * v + w' * v + v'*w - v'*w') / (W - w + w') = (V - v')*w'
+     * ===> (v' - v)*(w' - w) / (W - w + w') = (V - v')*w'
+     * ===> (v' - v)*(w' - w) = (V - v')*w'*(W - w + w')
+     *
+     * Since we are doing at 10-lag point which means V = v, we
+     * can simplify (3):
+     * ===> (v' - v)*(w' - w) = (v - v')*w'*(W - w + w')
+     * ===> (v' - v)*(w' - w) = -(v' - v)*w'*(W - w + w')
+     * ===> (v' - v)*(w' - w + w'*(W - w + w')) = 0
+     * ===> (v' - v)*(w'*(W - w + w') - w) = 0
+     * ===> (v' - v)*(w'*W - w) = 0
+     * ===> v' = v  or  w'*W = w (re-weight indicates w' != w)
+     *
+     * So the cfs.rq contains only one entity, hence vruntime of
+     * the entity (v) should always equal to the cfs.rq's weighted
+     * average vruntime (v'), which means we will always re-weight
+     * at 0-lag point, thus breach assumption. Proof completed.
+     */
 }
