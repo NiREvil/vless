7802fbf0d8c155a20e3f04b1afb817195fbc3d99
Diana <135276904+Diana-Cl@users.noreply.github.com>
2025-06-30 15:27:19 +0000

    style: apply code formatting and linting standards
    Applied automatic formatting using Prettier/Black and linting using ESLint/Ruff

--- edge/assets/SD.js
+++ edge/assets/SD.js
@@ -3,38 +3,38 @@ export default {
     let url = new URL(request.url);
     const path = url.pathname;
     const params = url.search;
-    
+
     // backend domain list
     let backendDomains = [
-      "creativecommons.org",
-      "go.inmobi.com",
-      "diana.nscl.ir",
-      "gur.gov.ua",
-      "fbi.gov",
-      "ip.sb",
-      "skk.moe",
-      "time.is",
-      "zula.ir",
-      "www.gov.ua",
-      "www.wto.org",
-      "www.csgo.com",
-      "www.cdnjs.com",
-      "www.iakeys.com",
-      "www.udacity.com",
-      "www.ipaddress.my",
-      "www.speedtest.net",
-      "www.ipchicken.com",
-      "www.glassdoor.com"
+      'creativecommons.org',
+      'go.inmobi.com',
+      'diana.nscl.ir',
+      'gur.gov.ua',
+      'fbi.gov',
+      'ip.sb',
+      'skk.moe',
+      'time.is',
+      'zula.ir',
+      'www.gov.ua',
+      'www.wto.org',
+      'www.csgo.com',
+      'www.cdnjs.com',
+      'www.iakeys.com',
+      'www.udacity.com',
+      'www.ipaddress.my',
+      'www.speedtest.net',
+      'www.ipchicken.com',
+      'www.glassdoor.com',
     ];
 
     // If HOST exists in environment variables, get new backend domain list using ADD function
     if (env.HOST) backendDomains = await ADD(env.HOST);
 
     // Get test path, default is '/sub'
-    let testPath = env.PATH || "/";
+    let testPath = env.PATH || '/';
     // Ensure test path starts with '/'
-    if (testPath.charAt(0) !== "/") testPath = "/" + testPath;
-    let responseCode = env.CODE || "200";
+    if (testPath.charAt(0) !== '/') testPath = '/' + testPath;
+    let responseCode = env.CODE || '200';
     // Log number of backend domains and their list
     console.log(
       `Backend count: ${backendDomains.length}\nBackend domains: ${backendDomains}\nTest path: ${testPath}\nResponse code: ${responseCode}`,
@@ -63,17 +63,13 @@ export default {
       // Loop while backend domain list is not empty
       while (backendDomains.length > 0) {
         // Randomly select a backend domain
-        const randomBackend =
-          backendDomains[Math.floor(Math.random() * backendDomains.length)];
+        const randomBackend = backendDomains[Math.floor(Math.random() * backendDomains.length)];
         // Remove selected domain from the list
-        backendDomains = backendDomains.filter(
-          (host) => host !== randomBackend,
-        );
+        backendDomains = backendDomains.filter(host => host !== randomBackend);
 
         url.hostname = randomBackend; // domain
-        url.pathname = testPath.split("?")[0];
-        url.search =
-          testPath.split("?")[1] == "" ? "" : "?" + testPath.split("?")[1];
+        url.pathname = testPath.split('?')[0];
+        url.search = testPath.split('?')[1] == '' ? '' : '?' + testPath.split('?')[1];
         try {
           // Make request with timeout
           const response = await fetchWithTimeout(new Request(url), {
@@ -81,7 +77,7 @@ export default {
           });
           // If response status is 200, request is successful
           if (response.status.toString() == responseCode) {
-            if (path != "/") url.pathname = path;
+            if (path != '/') url.pathname = path;
             console.log(`Using backend: ${url.hostname}`);
             //console.log(`Failed backends: ${failedBackends}`);
             console.log(`Remaining backends: ${backendDomains}`);
@@ -96,9 +92,9 @@ export default {
         }
       }
       // If all backends fail, throw error
-      return new Response("All backends are unavailable!", {
+      return new Response('All backends are unavailable!', {
         status: 404,
-        headers: { "content-type": "text/plain; charset=utf-8" },
+        headers: { 'content-type': 'text/plain; charset=utf-8' },
       });
     }
 
@@ -110,15 +106,14 @@ export default {
 async function ADD(envadd) {
   // Replace tabs, double quotes, single quotes and newlines with commas
   // Then replace multiple consecutive commas with single comma
-  var addtext = envadd.replace(/[ |"'\r\n]+/g, ",").replace(/,+/g, ",");
+  var addtext = envadd.replace(/[ |"'\r\n]+/g, ',').replace(/,+/g, ',');
 
   // Remove leading and trailing commas (if any)
-  if (addtext.charAt(0) == ",") addtext = addtext.slice(1);
-  if (addtext.charAt(addtext.length - 1) == ",")
-    addtext = addtext.slice(0, addtext.length - 1);
+  if (addtext.charAt(0) == ',') addtext = addtext.slice(1);
+  if (addtext.charAt(addtext.length - 1) == ',') addtext = addtext.slice(0, addtext.length - 1);
 
   // Split string by comma to get address array
-  const add = addtext.split(",");
+  const add = addtext.split(',');
 
   return add;
 }
